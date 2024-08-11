34cbe4835403fd2d2982aef01edf59b9b8786923
Benjamin Cassatt <benjamin.cassatt@gmail.com>
2024-08-11 14:12:08 -0400

    important changes

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.urandom(64)
     CLIENT_ID = os.environ.get('CLIENT_ID')
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-    REDIRECT_URI = 'http://localhost:5000/callback'
+    REDIRECT_URI = 'https://flask-service.b2hrcq1s6o076.us-west-2.cs.amazonlightsail.com/callback'
     SCOPE = 'user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private playlist-modify-public playlist-modify-private user-modify-playback-state user-read-playback-state'
     SESSION_TYPE = 'filesystem'
     OPEN_API_KEY = os.environ.get('OPENAI_API_KEY')
