eb2010cbde5fd92039ad24f3d863cf5354b8d698
Benjamin Cassatt <benjamin.cassatt@gmail.com>
2024-08-10 21:57:14 -0400

    hiding secrets

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(64)
-    CLIENT_ID = '194ca0faa98d44e9b9b9940785c64e8d'
-    CLIENT_SECRET = '253d7220dbc84f02ba9d53c75a0bf29d'
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     REDIRECT_URI = 'http://localhost:5000/callback'
     SCOPE = 'user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private playlist-modify-public playlist-modify-private user-modify-playback-state user-read-playback-state'
     SESSION_TYPE = 'filesystem'
